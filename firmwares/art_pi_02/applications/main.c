9d57028ae382b36449f3057e959b5945145c4af0
luhuadong <870179822@qq.com>
2022-01-27 08:20:14 +0800

    [fw] add ART-Pi GP2Y10 test

--- firmwares/art_pi_02/applications/main.c
+++ firmwares/art_pi_02/applications/main.c
@@ -12,13 +12,17 @@
 #include <rtdevice.h>
 #include "drv_common.h"
 #include "dhtxx.h"
+#include "gp2y10.h"
 
 #define LED_PIN GET_PIN(I, 8)
 #define DATA_PIN GET_PIN(I, 5)
 
+#define GP2Y10_ILED_PIN          GET_PIN(C, 7)
+#define GP2Y10_AOUT_PIN          GET_PIN(B, 1)
+
 int main(void)
 {
-    rt_uint32_t count = 10;
+    rt_uint32_t count = 3;
     rt_device_t temp_sensor = RT_NULL;
     struct rt_sensor_data sensor_data;
 
@@ -50,6 +54,71 @@ static int rt_hw_dht_port(void)
     return RT_EOK;
 }
 INIT_COMPONENT_EXPORT(rt_hw_dht_port);
+
+
+static void read_dust_entry(void *args)
+{
+    rt_device_t dust_dev = RT_NULL;
+    struct rt_sensor_data sensor_data;
+
+    dust_dev = rt_device_find(args);
+    if (dust_dev == RT_NULL)
+    {
+        rt_kprintf("Can't find dust device.\n");
+        return;
+    }
+
+    if (rt_device_open(dust_dev, RT_DEVICE_FLAG_RDWR))
+    {
+        rt_kprintf("Open dust device failed.\n");
+        return;
+    }
+
+    while(1)
+    {
+        if (1 != rt_device_read(dust_dev, 0, &sensor_data, 1))
+        {
+            rt_kprintf("Read dust data failed.\n");
+            continue;
+        }
+        rt_kprintf("[%d] Dust: %d\n", sensor_data.timestamp, sensor_data.data.dust);
+
+        rt_thread_mdelay(2000);
+    }
+
+    rt_device_close(dust_dev);
+}
+
+static int gp2y10_read_sample(void)
+{
+    rt_thread_t dust_thread;
+
+    dust_thread = rt_thread_create("dust_th", read_dust_entry,
+                                   "dust_gp2", 1024,
+                                    RT_THREAD_PRIORITY_MAX / 2, 20);
+
+    if (dust_thread)
+        rt_thread_startup(dust_thread);
+}
+#ifdef FINSH_USING_MSH
+MSH_CMD_EXPORT(gp2y10_read_sample, read gp2y10 sensor data);
+#endif
+
+static int rt_hw_gp2y10_port(void)
+{
+    struct gp2y10_device gp2y10_dev;
+    struct rt_sensor_config cfg;
+
+    gp2y10_dev.iled_pin = GP2Y10_ILED_PIN;
+    gp2y10_dev.aout_pin = GP2Y10_AOUT_PIN;
+
+    //cfg.intf.type = RT_SENSOR_INTF_ADC;
+    cfg.intf.user_data = (void *)&gp2y10_dev;
+    rt_hw_gp2y10_init("gp2", &cfg);
+
+    return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(rt_hw_gp2y10_port);
 
 #include "stm32h7xx.h"
 static int vtor_config(void)
