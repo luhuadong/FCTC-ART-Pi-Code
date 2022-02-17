5960bcbddf047ab5b7441355f7256eae1f60b043
luhuadong <870179822@qq.com>
2022-02-18 02:35:14 +0800

    [fw] finish art_pi_01

--- firmwares/art_pi_01/applications/main.c
+++ firmwares/art_pi_01/applications/main.c
@@ -12,23 +12,61 @@
 #include <rtdevice.h>
 #include "drv_common.h"
 
-#define LED_PIN GET_PIN(I, 8)
+#define LED_B_PIN      GET_PIN(I, 8)
+#define LED_R_PIN      GET_PIN(C, 15)
+#define USER_KEY1_PIN  GET_PIN(H, 4)
+
+void key_pressed(void *args)
+{
+    static rt_uint8_t flag = 0;
+    rt_kprintf("key pressed! %d\n", flag);
+
+    switch(flag) {
+    case 0: {
+            rt_pin_write(LED_B_PIN, PIN_LOW);
+            rt_pin_write(LED_R_PIN, PIN_HIGH);
+            flag++;
+            break;
+        }
+    case 1: {
+            rt_pin_write(LED_B_PIN, PIN_HIGH);
+            rt_pin_write(LED_R_PIN, PIN_LOW);
+            flag++;
+            break;
+        }
+    case 2: {
+            rt_pin_write(LED_B_PIN, PIN_LOW);
+            rt_pin_write(LED_R_PIN, PIN_LOW);
+            flag++;
+            break;
+        }
+    case 3: {
+            rt_pin_write(LED_B_PIN, PIN_HIGH);
+            rt_pin_write(LED_R_PIN, PIN_HIGH);
+            flag = 0;
+            break;
+        }
+    default: break;
+    }
+}
 
 int main(void)
 {
-    rt_uint32_t count = 1;
+    /* LED 引脚为输出模式 */
+    rt_pin_mode(LED_B_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_R_PIN, PIN_MODE_OUTPUT);
 
-    rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+    /* 初始状态 LED 不亮 */
+    rt_pin_write(LED_B_PIN, PIN_HIGH);
+    rt_pin_write(LED_R_PIN, PIN_HIGH);
 
-    rt_kprintf("FCTC Demo 01\n");
+    /* 按键 KEY1 引脚为输入模式 */
+    rt_pin_mode(USER_KEY1_PIN, PIN_MODE_INPUT_PULLUP);
+    /* 绑定中断，下降沿模式，回调函数名为 key_pressed */
+    rt_pin_attach_irq(USER_KEY1_PIN, PIN_IRQ_MODE_FALLING, key_pressed, RT_NULL);
+    /* 使能中断 */
+    rt_pin_irq_enable(USER_KEY1_PIN, PIN_IRQ_ENABLE);
 
-    while(count++)
-    {
-        rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_LOW);
-    }
     return RT_EOK;
 }
 
