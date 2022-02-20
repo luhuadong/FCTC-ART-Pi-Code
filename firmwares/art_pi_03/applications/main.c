b451f524db1c3d9d3dbb211f5fa6c0a427ccf6da
luhuadong <870179822@qq.com>
2022-02-21 04:56:13 +0800

    [fw] finish RS-232 test

--- firmwares/art_pi_03/applications/main.c
+++ firmwares/art_pi_03/applications/main.c
@@ -5,7 +5,7 @@
  *
  * Change Logs:
  * Date           Author       Notes
- * 2020-09-02     RT-Thread    first version
+ * 2021-06-02     Rudy Lo      first version
  */
 
 #include <rtthread.h>
@@ -13,36 +13,60 @@
 #include "drv_common.h"
 #include "pmsxx.h"
 
-#define UART_NAME   "uart6"
+#define UART_NAME      "uart6"
+
+#define PASSIVE_MODE   1  /* 0: Sensor -> MCU, 1: MCU -> Sensor */
 
 int main(void)
 {
-    rt_uint16_t loop = 10;
+    int ret = 0;
     struct pms_response resp;
+
     pms_device_t sensor = pms_create(UART_NAME);
-    if (!sensor)
-    {
-        rt_kprintf("(PMS) Init failed\n");
-        return;
+
+    if (!sensor) {
+        rt_kprintf("Init PMS5003ST sensor failed!\n");
+        return -1;
     }
 
-    while (!pms_is_ready(sensor))
-    {
+#if 0
+    while (!pms_is_ready(sensor)) {
+        rt_kprintf("wait ready...\n");
         rt_thread_mdelay(1000);
     }
+#endif
 
+#if 1
     pms_set_mode(sensor, PMS_MODE_NORMAL);
+#if PASSIVE_MODE
     pms_set_mode(sensor, PMS_MODE_PASSIVE);
+#else
+    pms_set_mode(sensor, PMS_MODE_ACTIVE);
+#endif
+#endif
 
     while (1)
     {
-        rt_kprintf("\n[%d] Request...\n", loop);
-        pms_read(sensor, &resp, sizeof(resp), rt_tick_from_millisecond(3000));
-        pms_show_response(&resp);
+        rt_memset(&resp, 0, sizeof(resp));
+
+#if PASSIVE_MODE
+        rt_kprintf("\nRequest...\n");
+        ret = pms_read(sensor, &resp, sizeof(resp), rt_tick_from_millisecond(3000));
+#else
+        rt_kprintf("\nWait...\n");
+        ret = pms_wait(sensor, &resp, sizeof(resp));
+#endif
+
+        if (ret == sizeof(resp)) {
+            pms_show_response(&resp);
+        }
+        else {
+            rt_kprintf("Read error\n");
+        }
+
         rt_thread_mdelay(3000);
     }
 
-    rt_kprintf("(PMS) Finished!\n");
     pms_delete(sensor);
 }
 
