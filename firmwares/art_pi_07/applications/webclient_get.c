238d2b0644ef5897686367d4bd9a85361d910b61
luhuadong <870179822@qq.com>
2022-03-04 12:05:07 +0800

    [fw] finish HTTP GET test

--- firmwares/art_pi_07/applications/webclient_get.c
+++ firmwares/art_pi_07/applications/webclient_get.c
@@ -11,12 +11,14 @@
 #include <rtthread.h>
 #include <webclient.h>
 
-#define GET_HEADER_BUFSZ               4096
-#define GET_RESP_BUFSZ                 4096
+#define GET_HEADER_BUFSZ           1024
+#define GET_RESP_BUFSZ             1024
 
-#define GET_LOCAL_URI                  "http://www.rt-thread.com/service/rt-thread.txt"
+#define HTTP_GET_URL               "http://www.rt-thread.com/service/rt-thread.txt"
 
-/* send HTTP GET request by common request interface, it used to receive longer data */
+
+/* send HTTP GET request by simplify request interface, it used to received shorter data */
+
 static int webclient_get_comm(const char *uri)
 {
     struct webclient_session* session = RT_NULL;
@@ -32,10 +34,9 @@ static int webclient_get_comm(const char *uri)
         rt_kprintf("no memory for receive buffer.\n");
         ret = -RT_ENOMEM;
         goto __exit;
-
     }
 
-    /* create webclient session and set header response size */
+    /* 创建 webclient 会话，设置响应头部大小 */
     session = webclient_session_create(GET_HEADER_BUFSZ);
     if (session == RT_NULL)
     {
@@ -43,12 +44,12 @@ static int webclient_get_comm(const char *uri)
         goto __exit;
     }
 
-    const unsigned char *key = "hello";
+    /* 拼接自定义头部数据 */
     webclient_request_header_add(&header, "Accept: application/json\r\n");
     webclient_request_header_add(&header, "User-Agent: RT-Thread HTTP Agent\r\n");
     webclient_request_header_add(&header, "Connection: close\r\n");
 
-    /* send GET request by default header */
+    /* 发送 GET 请求 */
     if ((resp_status = webclient_get(session, uri)) != 200)
     {
         rt_kprintf("webclient GET request failed, response(%d) error.\n", resp_status);
@@ -61,158 +62,95 @@ static int webclient_get_comm(const char *uri)
     content_length = webclient_content_length_get(session);
     if (content_length < 0)
     {
+        /* 获取失败 */
         rt_kprintf("webclient GET request type is chunked.\n");
-        do
-        {
-            bytes_read = webclient_read(session, (void *)buffer, GET_RESP_BUFSZ);
-            if (bytes_read <= 0)
-            {
-                break;
-            }
-
-            for (index = 0; index < bytes_read; index++)
-            {
-                rt_kprintf("%c", buffer[index]);
-            }
-        } while (1);
-
-        rt_kprintf("\n");
+        goto __exit;
     }
-    else
+
+    int content_pos = 0;
+
+    do
     {
-        int content_pos = 0;
+        bytes_read = webclient_read(session, (void *)buffer,
+                                    content_length - content_pos > GET_RESP_BUFSZ ?
+                                    GET_RESP_BUFSZ : content_length - content_pos);
+        if (bytes_read <= 0) {
+            /* 读取完毕 */
+            break;
+        }
 
-        do
-        {
-            bytes_read = webclient_read(session, (void *)buffer,
-                    content_length - content_pos > GET_RESP_BUFSZ ?
-                            GET_RESP_BUFSZ : content_length - content_pos);
-            if (bytes_read <= 0)
-            {
-                break;
-            }
+        /* 打印响应数据 */
+        for (index = 0; index < bytes_read; index++) {
+            rt_kprintf("%c", buffer[index]);
+        }
+        content_pos += bytes_read;
 
-            for (index = 0; index < bytes_read; index++)
-            {
-                rt_kprintf("%c", buffer[index]);
-            }
+    } while (content_pos < content_length);
 
-            content_pos += bytes_read;
-        } while (content_pos < content_length);
-
-        rt_kprintf("\n");
-    }
+    rt_kprintf("\n");
 
 __exit:
     if (session)
-    {
         webclient_close(session);
-    }
 
     if (buffer)
-    {
         web_free(buffer);
-    }
 
     return ret;
 }
 
-/* send HTTP GET request by simplify request interface, it used to received shorter data */
-static int webclient_get_smpl(const char *uri)
+static int webclient_get_data(const char *uri)
 {
+    char *header = RT_NULL;
     char *response = RT_NULL;
     size_t resp_len = 0;
-    int index;
 
-    if (webclient_request(uri, RT_NULL, RT_NULL, 0, (void **)&response, &resp_len) < 0)
-    {
-        rt_kprintf("webclient send get request failed.");
+    /* 拼接自定义头部数据 */
+    webclient_request_header_add(&header, "User-Agent: GetIoT HTTP Agent\r\n");
+
+    /* 发送请求，第3个参数为NULL表示GET请求 */
+    if (webclient_request(uri, header, RT_NULL, 0, (void **)&response, &resp_len) < 0) {
+        rt_kprintf("webclient GET request response data error.\n");
         return -RT_ERROR;
     }
 
-    rt_kprintf("webclient send get request by simplify request interface.\n");
     rt_kprintf("webclient get response data: \n");
-    for (index = 0; index < rt_strlen(response); index++)
-    {
+
+    /* 打印响应数据 */
+    for (int index = 0; index < resp_len; index++) {
         rt_kprintf("%c", response[index]);
     }
     rt_kprintf("\n");
 
-    if (response)
-    {
+    /* 释放内存 */
+    if (response) {
         web_free(response);
     }
 
     return 0;
 }
 
-
-int webclient_get_test(int argc, char **argv)
+int http_get_test(int argc, char **argv)
 {
     char *uri = RT_NULL;
 
-    if (argc == 1)
-    {
-        uri = web_strdup(GET_LOCAL_URI);
-        if(uri == RT_NULL)
-        {
-            rt_kprintf("no memory for create get request uri buffer.\n");
-            return -RT_ENOMEM;
-        }
-
-        webclient_get_comm(uri);
+    /* 如果没有输入URI，则使用默认的测试地址 */
+    if (argc == 1) {
+        uri = web_strdup(HTTP_GET_URL);
     }
-    else if (argc == 2)
-    {
-        if (rt_strcmp(argv[1], "-s") == 0)
-        {
-            uri = web_strdup(GET_LOCAL_URI);
-            if(uri == RT_NULL)
-            {
-                rt_kprintf("no memory for create get request uri buffer.\n");
-                return -RT_ENOMEM;
-            }
-
-            webclient_get_smpl(uri);
-        }
-        else
-        {
-            uri = web_strdup(argv[1]);
-            if(uri == RT_NULL)
-            {
-                rt_kprintf("no memory for create get request uri buffer.\n");
-                return -RT_ENOMEM;
-            }
-            webclient_get_comm(uri);
-        }
+    else if (argc == 2) {
+        uri = web_strdup(argv[1]);
     }
-    else if(argc == 3 && rt_strcmp(argv[1], "-s") == 0)
-    {
-        uri = web_strdup(argv[2]);
-        if(uri == RT_NULL)
-        {
-            rt_kprintf("no memory for create get request uri buffer.\n");
-            return -RT_ENOMEM;
-        }
-
-        webclient_get_smpl(uri);
-    }
-    else
-    {
-        rt_kprintf("web_get_test [URI]     - webclient GET request test.\n");
-        rt_kprintf("web_get_test -s [URI]  - webclient simplify GET request test.\n");
+    else {
+        rt_kprintf("Usage: %s [uri]\n", argv[0]);
         return -RT_ERROR;
     }
 
-    if (uri)
-    {
-        web_free(uri);
-    }
+    /* 发送GET请求并获取响应数据 */
+    webclient_get_data(uri);
+    /* 释放资源 */
+    web_free(uri);
 
     return RT_EOK;
 }
-
-#ifdef FINSH_USING_MSH
-#include <finsh.h>
-MSH_CMD_EXPORT(webclient_get_test, webclient get request test);
-#endif /* FINSH_USING_MSH */
+MSH_CMD_EXPORT(http_get_test, webclient get request test);
